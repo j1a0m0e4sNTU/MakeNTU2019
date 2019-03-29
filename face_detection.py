41af036f7946ff1f9b6bc7d9b903ff7e7e2c1a9b
James Lin <b04901069@ntu.edu.tw>
2019-03-29 14:42:26 +0800

    refactorize face_detector

--- face_detection.py
+++ face_detection.py
@@ -1,23 +1,22 @@
 import cv2 as cv
 import numpy as np
 
-def get_face_detector():
-    face_detector = cv.CascadeClassifier('./filters/haarcascade_frontalface_default.xml')
-
 class Face_detector():
-    def __init__(self, path= './filters/haarcascade_frontalface_default.xml'):
+    def __init__(self, path= './filters/haarcascade_frontalface_default.xml', scaleFactor= 2, minNeighobrs= 3):
         self.detector = cv.CascadeClassifier(path)
+        self.scaleFactor = scaleFactor
+        self.minNeighobrs = minNeighobrs
 
     def mark_face(self, img):
         img_gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-        faces = self.detector.detectMultiScale(img_gray, 2, 5)
+        faces = self.detector.detectMultiScale(img_gray, scaleFactor= self.scaleFactor, minNeighbors= self.minNeighobrs)
        
         for (x,y,w,h) in faces:
-            cv.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
+            cv.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 5)
 
     def has_face(self, img):
         img_gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-        faces = self.detector.detectMultiScale(img_gray, 2, 5)
+        faces = self.detector.detectMultiScale(img_gray, scaleFactor= self.scaleFactor, minNeighbors= self.minNeighobrs)
         
         if (len(faces) > 0):
             return True
