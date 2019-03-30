8f5a4db45d5925b49393e08343232d9a36d5b038
James Lin <b04901069@ntu.edu.tw>
2019-03-30 16:57:08 +0800

    add button

--- button.py
+++ button.py
@@ -1,6 +1,17 @@
 import RPi.GPIO as GPIO
+import sys
+
 
 class Button():
-    def __init__(self, pin):
+    def __init__(self, pin, call_back):
         self.pin = pin
+        GPIO.setup(self.pin, GPIO.IN, pull_up_down= GPIO.PUD_UP)
+        GPIO.add_event_detect(self.pin, GPIO.RISING, callback= call_back, bouncetime= 300)
+
+if __name__ == '__main__':
+    print('- Button -')
+    pin = int(sys.argv[1])
+    def f():
+        print('push')
+    button = Button(pin, f)
 
