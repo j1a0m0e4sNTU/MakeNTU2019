d9291fb131351a47e775f1115e67947a6b7662fc
James Lin <b04901069@ntu.edu.tw>
2019-03-30 20:09:06 +0800

    add

--- test/diff_test.py
+++ test/diff_test.py
@@ -8,6 +8,13 @@ parser.add_argument('mode', choices=['shot', 'diff'])
 parser.add_argument('-name', choices=['0','1'])
 args = parser.parse_args()
 
+def normal(name):
+    img = cv.imread(name)
+    img_s = img[:, :, 0] + img[:, :, 1] + img[:, :, 2]
+    mean = np.mean(img_s)
+    img_n = img_s - mean
+    return img_n 
+
 if __name__ == '__main__':
     if args.mode == 'shot':
         cam = PiCamera()
@@ -15,9 +22,9 @@ if __name__ == '__main__':
         cam.capture(name)
         print('saved', name)
     else:
-        img_0 = cv.imread('0.jpg')
-        img_1 = cv.imread('1.jpg')
+        img_0 = normal('0.jpg')
+        img_1 = normal('1.jpg')
 
-        diff = cv.absdiff(img_0, img_1)
+        diff = np.sum(img_0 - img_1)
         s = np.sum(diff)
         print('diff:', s)
