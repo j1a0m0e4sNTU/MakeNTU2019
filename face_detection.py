35663059dde718ebef71a0420f03407cc8c8f15d
James Lin <b04901069@ntu.edu.tw>
2019-03-31 08:01:35 +0800

    test face_detection

--- face_detection.py
+++ face_detection.py
@@ -1,5 +1,6 @@
 import cv2 as cv
 import numpy as np
+from LED import LED
 
 class Face_detector():
     def __init__(self, path= './filters/haarcascade_frontalface_default.xml', scaleFactor= 2, minNeighobrs= 3):
@@ -25,13 +26,23 @@ class Face_detector():
 
 
 if __name__ == '__main__':
+    import argparse
+    parser.add_argument('-s', type= int, default= 2)
+    parser.add_argument('-m', type= int, default= 3)
+    args = parser.parse_args()
+
     cam = cv.VideoCapture(0)
-    face_detector = Face_detector()
+    face_detector = Face_detector(args.s, args.m)
+    green_led = LED(5)
     while True:
         _, img = cam.read()
-        face_detector.mark_face(img)
-        cv.imshow('Face detection', img)
-        
+        # face_detector.mark_face(img)
+        # cv.imshow('Face detection', img)
+        if detector.has_face(img):
+            green_led.turn_on()
+        else:
+            green_led.turn_off()
+
         if cv.waitKey(1) == ord('q'):
             break
 
